d7fcac49248de1dc1f830c566682459af9b7871f
otter <zvrivasca@unitru.edu.pe>
2025-03-14 19:21:11 -0500

    Agregar archivos

---
+++ app.js
@@ -0,0 +1,19 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Agregar amigo// debe validas que sea diferente de vacío y que se ingrese en un array.
+const listaAmigos = [];
+
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nombre = inputAmigo.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, ingresa un nombre válido.');
+        return;
+    }
+
+    listaAmigos.push(nombre);
+    inputAmigo.value = '';
+
+    mostrarLista();
+}
